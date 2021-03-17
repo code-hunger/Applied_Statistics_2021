289b0a79c738158d07900a58d8713f9a0625da97
Code Hunger <beinghavingbreackfast@gmail.com>
2021-03-17 17:15:27 +0200

    Create an "in.circle" function for prettier code

--- week3/mysols.r
+++ week3/mysols.r
@@ -1,14 +1,18 @@
+in.circle <- function (x, y, r) {
+    return (x^2 + y^2 <= r)
+}
+
 square.approx.Pi <- function (trials) {
     x = runif(trials, min = -1, max = +1)
     y = runif(trials, min = -1, max = +1)
 
     m = matrix(c(x,y),,2)
-    inner = m[m[,1] ^2 + m[,2]^2 <= 1]
-    outer = m[m[,1] ^2 + m[,2]^2 > 1]
+    inner = m[in.circle(m[,1], m[,2], 1)]
+    outer = m[!in.circle(m[,1], m[,2], 1)]
     plot(matrix(outer,,2), col = "red")
     points(matrix(inner,,2), col = "green")
 
-    return (4*sum(sqrt(x^2 + y^2) <= 1)/trials)
+    return (4*sum(in.circle(x,y,1))/trials)
 }
 
 square.approx.Pi(5000)
